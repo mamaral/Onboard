add3a705e77da068613ae819698239df649f2b68
Mike Amaral <mike.amaral@ping4.com>
2014-08-19 12:56:08 -0400

    Added constants where applicable.

--- Onboard/OnboardingContentViewController.m
+++ Onboard/OnboardingContentViewController.m
@@ -8,6 +8,15 @@
 
 #import "OnboardingContentViewController.h"
 
+static NSString * const kDefaultOnboardingFont = @"Helvetica-Light";
+
+static CGFloat const kContentWidthMultiplier = 0.9;
+static CGFloat const kDefaultOnboardingPadding = 20;
+static CGFloat const kOnboardingTitleFontSize = 38;
+static CGFloat const kOnboardingBodyFontSize = 28;
+static CGFloat const kActionButtonHeight = 50;
+static CGFloat const kMainPageControlHeight = 35;
+
 @interface OnboardingContentViewController ()
 
 @end
@@ -37,36 +46,35 @@
     
     CGFloat viewWidth = CGRectGetWidth(self.view.frame);
     CGFloat horizontalCenter = viewWidth / 2;
-    CGFloat contentWidth = viewWidth * 0.9;
-    CGFloat padding = 20;
+    CGFloat contentWidth = viewWidth * kContentWidthMultiplier;
     
     UIImageView *imageView = [[UIImageView alloc] initWithImage:_image];
-    [imageView setFrame:CGRectMake((viewWidth / 2) - (100 / 2), 4 * padding , 100, 100)];
+    [imageView setFrame:CGRectMake((viewWidth / 2) - (100 / 2), 4 * kDefaultOnboardingPadding , 100, 100)];
     [self.view addSubview:imageView];
     
-    UILabel *mainTextLabel = [[UILabel alloc] initWithFrame:CGRectMake(padding, CGRectGetMaxY(imageView.frame) + padding, contentWidth, 0)];
+    UILabel *mainTextLabel = [[UILabel alloc] initWithFrame:CGRectMake(kDefaultOnboardingPadding, CGRectGetMaxY(imageView.frame) + kDefaultOnboardingPadding, contentWidth, 0)];
     mainTextLabel.text = _titleText;
     mainTextLabel.textColor = [UIColor whiteColor];
-    mainTextLabel.font = [UIFont fontWithName:@"Helvetica-Light" size:38];
+    mainTextLabel.font = [UIFont fontWithName:kDefaultOnboardingFont size:kOnboardingTitleFontSize];
     mainTextLabel.numberOfLines = 0;
     mainTextLabel.textAlignment = NSTextAlignmentCenter;
     [mainTextLabel sizeToFit];
     mainTextLabel.center = CGPointMake(horizontalCenter, mainTextLabel.center.y);
     [self.view addSubview:mainTextLabel];
     
-    UILabel *subTextLabel = [[UILabel alloc] initWithFrame:CGRectMake(padding, CGRectGetMaxY(mainTextLabel.frame) + padding, contentWidth, 0)];
+    UILabel *subTextLabel = [[UILabel alloc] initWithFrame:CGRectMake(kDefaultOnboardingPadding, CGRectGetMaxY(mainTextLabel.frame) + kDefaultOnboardingPadding, contentWidth, 0)];
     subTextLabel.text = _body;
     subTextLabel.textColor = [UIColor whiteColor];
-    subTextLabel.font = [UIFont fontWithName:@"Helvetica-Light" size:28];
+    subTextLabel.font = [UIFont fontWithName:kDefaultOnboardingFont size:kOnboardingBodyFontSize];
     subTextLabel.numberOfLines = 0;
     subTextLabel.textAlignment = NSTextAlignmentCenter;
     [subTextLabel sizeToFit];
     subTextLabel.center = CGPointMake(horizontalCenter, subTextLabel.center.y);
     [self.view addSubview:subTextLabel];
     
-    // create the action button
+    // create the action button if we were given button text
     if (_buttonText) {
-        UIButton *actionButton = [[UIButton alloc] initWithFrame:CGRectMake((CGRectGetMaxX(self.view.frame) / 2) - (contentWidth / 2), CGRectGetMaxY(self.view.frame) - 100 - padding, contentWidth, 50)];
+        UIButton *actionButton = [[UIButton alloc] initWithFrame:CGRectMake((CGRectGetMaxX(self.view.frame) / 2) - (contentWidth / 2), CGRectGetMaxY(self.view.frame) - kMainPageControlHeight - kActionButtonHeight - kDefaultOnboardingPadding, contentWidth, kActionButtonHeight)];
         actionButton.titleLabel.font = [UIFont systemFontOfSize:24];
         [actionButton setTitle:_buttonText forState:UIControlStateNormal];
         [actionButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
