b9b439fa20424735f503f4dd34244e63e5cf5aee
Enrique Juan de Dios <juandedioz@gmail.com>
2016-04-23 16:13:45 -0500

    Add properties for modify the following Action Button properties: Height, Padding Width (Related with the container), Background Color.

--- Source/OnboardingContentViewController.m
+++ Source/OnboardingContentViewController.m
@@ -13,6 +13,10 @@
 static NSString * const kDefaultOnboardingFont = @"Helvetica-Light";
 
 #define DEFAULT_TEXT_COLOR [UIColor whiteColor];
+#define DEFAULT_BORDER_RADIUS 0.0;
+#define DEFAULT_BUTTON_BACKGROUND_COLOR [UIColor clearColor];
+#define DEFAULT_BUTTON_PADDING_WIDTH 0.0;
+#define DEFAULT_BUTTON_HEIGHT 50.0;
 
 static CGFloat const kContentWidthMultiplier = 0.9;
 static CGFloat const kDefaultImageViewSize = 100;
@@ -25,7 +29,6 @@ static CGFloat const kDefaultTitleFontSize = 38;
 static CGFloat const kDefaultBodyFontSize = 28;
 static CGFloat const kDefaultButtonFontSize = 24;
 
-static CGFloat const kActionButtonHeight = 50;
 static CGFloat const kMainPageControlHeight = 35;
 
 NSString * const kOnboardMainTextAccessibilityIdentifier = @"OnboardMainTextAccessibilityIdentifier";
@@ -94,73 +97,77 @@ NSString * const kOnboardActionButtonAccessibilityIdentifier = @"OnboardActionBu
     self.videoURL = videoURL;
 
     self.buttonActionHandler = actionBlock;
-    
+
     // default auto-navigation
     self.movesToNextViewController = NO;
-    
+
     // default icon properties
     if(_image) {
-		self.iconHeight = _image.size.height;
-		self.iconWidth = _image.size.width;
-	}
-    
+        self.iconHeight = _image.size.height;
+        self.iconWidth = _image.size.width;
+    }
+
     else {
-		self.iconHeight = kDefaultImageViewSize;
-		self.iconWidth = kDefaultImageViewSize;
-	}
-    
+        self.iconHeight = kDefaultImageViewSize;
+        self.iconWidth = kDefaultImageViewSize;
+    }
+
     // default title properties
     self.titleFontName = kDefaultOnboardingFont;
     self.titleFontSize = kDefaultTitleFontSize;
-    
+
     // default body properties
     self.bodyFontName = kDefaultOnboardingFont;
     self.bodyFontSize = kDefaultBodyFontSize;
-    
+
     // default button properties
     self.buttonFontName = kDefaultOnboardingFont;
     self.buttonFontSize = kDefaultButtonFontSize;
-    
+
     // default padding values
     self.topPadding = kDefaultTopPadding;
     self.underIconPadding = kDefaultUnderIconPadding;
     self.underTitlePadding = kDefaultUnderTitlePadding;
     self.bottomPadding = kDefaultBottomPadding;
     self.underPageControlPadding = kDefaultUnderPageControlPadding;
-    
+
     // default colors
     self.titleTextColor = DEFAULT_TEXT_COLOR;
     self.bodyTextColor = DEFAULT_TEXT_COLOR;
     self.buttonTextColor = DEFAULT_TEXT_COLOR;
-    
+    self.buttonPaddingWidth = DEFAULT_BUTTON_PADDING_WIDTH;
+    self.buttonHeight = DEFAULT_BUTTON_HEIGHT;
+    self.buttonBorderRadius = DEFAULT_BORDER_RADIUS;
+    self.buttonBackgroundColor = DEFAULT_BUTTON_BACKGROUND_COLOR;
+
     // default blocks
     self.viewWillAppearBlock = ^{};
     self.viewDidAppearBlock = ^{};
     self.viewWillDisappearBlock = ^{};
     self.viewDidDisappearBlock = ^{};
-    
+
     // Handle when the app enters the foreground.
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(handleAppEnteredForeground) name:UIApplicationWillEnterForegroundNotification object:nil];
-    
+
     return self;
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+
     // now that the view has loaded we can generate the content
     [self generateView];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
-    
+
     // if we have a delegate set, mark ourselves as the next page now that we're
     // about to appear
     if (self.delegate) {
         [self.delegate setNextPage:self];
     }
-    
+
     // call our view will appear block
     if (self.viewWillAppearBlock) {
         dispatch_async(dispatch_get_main_queue(), ^{
@@ -171,20 +178,20 @@ NSString * const kOnboardActionButtonAccessibilityIdentifier = @"OnboardActionBu
 
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
-    
+
     // if we have a delegate set, mark ourselves as the current page now that
     // we've appeared
     if (self.delegate) {
         [self.delegate setCurrentPage:self];
     }
-    
+
     // call our view did appear block
     if (self.viewDidAppearBlock) {
         dispatch_async(dispatch_get_main_queue(), ^{
             self.viewDidAppearBlock();
         });
     }
-    
+
     // if we have a video, start playing
     if (self.moviePlayerController.playbackState != MPMoviePlaybackStatePlaying) {
         self.moviePlayerController.currentPlaybackTime = 0.0;
@@ -212,7 +219,7 @@ NSString * const kOnboardActionButtonAccessibilityIdentifier = @"OnboardActionBu
             self.viewDidDisappearBlock();
         });
     }
-    
+
     // if we have a video, stop playing
     if (self.moviePlayerController.playbackState != MPMoviePlaybackStateStopped) {
         [self.moviePlayerController stop];
@@ -224,43 +231,41 @@ NSString * const kOnboardActionButtonAccessibilityIdentifier = @"OnboardActionBu
 }
 
 - (void)generateView {
-    // If no background color specified,
     // we want our background to be clear so we can see through it to the image provided
-    self.view.backgroundColor = self.backgroundColor ?
-        self.backgroundColor : [UIColor clearColor];
-    
+    self.view.backgroundColor = [UIColor clearColor];
+
     // do some calculation for some common values we'll need, namely the width of the view,
     // the center of the width, and the content width we want to fill up, which is some
     // fraction of the view width we set in the multipler constant
     CGFloat viewWidth = CGRectGetWidth(self.view.frame);
     CGFloat horizontalCenter = viewWidth / 2;
     CGFloat contentWidth = viewWidth * kContentWidthMultiplier;
-    
+
     if (_image) {
-        
+
         // create the image view with the appropriate image, size, and center in on screen
         _imageView = [[UIImageView alloc] initWithImage:_image];
         [_imageView setFrame:CGRectMake(horizontalCenter - (self.iconWidth / 2), self.topPadding, self.iconWidth, self.iconHeight)];
         [self.view addSubview:_imageView];
-    
+
     } else if (self.videoURL) {
-        
+
         // or if we have a video create and configure the video player controller
         self.moviePlayerController = [MPMoviePlayerController new];
         self.moviePlayerController.contentURL = self.videoURL;
         self.moviePlayerController.view.frame = self.view.frame;
         self.moviePlayerController.repeatMode = MPMovieRepeatModeOne;
         self.moviePlayerController.controlStyle = MPMovieControlStyleNone;
-        
+
         UIImageView *thumbnailImageView = [[UIImageView alloc] initWithImage:[self thumbnailImageForVideo:self.videoURL]];
         thumbnailImageView.frame = self.view.frame;
         thumbnailImageView.contentMode = UIViewContentModeScaleAspectFit;
-        
+
         [self.moviePlayerController.backgroundView addSubview:thumbnailImageView];
-        
+
         [self.view addSubview:self.moviePlayerController.view];
     }
-    
+
     // create and configure the main text label sitting underneath the icon with the provided padding
     _mainTextLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, CGRectGetMaxY(_imageView.frame) + self.underIconPadding, contentWidth, 0)];
     _mainTextLabel.accessibilityIdentifier = kOnboardMainTextAccessibilityIdentifier;
@@ -272,7 +277,7 @@ NSString * const kOnboardActionButtonAccessibilityIdentifier = @"OnboardActionBu
     [_mainTextLabel sizeToFit];
     _mainTextLabel.center = CGPointMake(horizontalCenter, _mainTextLabel.center.y);
     [self.view addSubview:_mainTextLabel];
-    
+
     // create and configure the sub text label
     _subTextLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, CGRectGetMaxY(_mainTextLabel.frame) + self.underTitlePadding, contentWidth, 0)];
     _subTextLabel.accessibilityIdentifier = kOnboardSubTextAccessibilityIdentifier;
@@ -284,12 +289,15 @@ NSString * const kOnboardActionButtonAccessibilityIdentifier = @"OnboardActionBu
     [_subTextLabel sizeToFit];
     _subTextLabel.center = CGPointMake(horizontalCenter, _subTextLabel.center.y);
     [self.view addSubview:_subTextLabel];
-    
+
+
     // create the action button if we were given button text
     if (_buttonText) {
-        _actionButton = [[UIButton alloc] initWithFrame:CGRectMake((CGRectGetMaxX(self.view.frame) / 2) - (contentWidth / 2), CGRectGetMaxY(self.view.frame) - self.underPageControlPadding - kMainPageControlHeight - kActionButtonHeight - self.bottomPadding, contentWidth, kActionButtonHeight)];
+        _actionButton = [[UIButton alloc] initWithFrame:CGRectMake((CGRectGetMaxX(self.view.frame) / 2) - ((contentWidth-self.buttonPaddingWidth) / 2), CGRectGetMaxY(self.view.frame) - self.underPageControlPadding - kMainPageControlHeight - self.buttonHeight - self.bottomPadding, contentWidth-self.buttonPaddingWidth, self.buttonHeight)];
         _actionButton.accessibilityIdentifier = kOnboardActionButtonAccessibilityIdentifier;
         _actionButton.titleLabel.font = [UIFont fontWithName:self.buttonFontName size:self.buttonFontSize];
+        _actionButton.backgroundColor = self.buttonBackgroundColor;
+        _actionButton.layer.cornerRadius = self.buttonBorderRadius;
         [_actionButton setTitle:_buttonText forState:UIControlStateNormal];
         [_actionButton setTitleColor:self.buttonTextColor forState:UIControlStateNormal];
         [_actionButton addTarget:self action:@selector(handleButtonPressed) forControlEvents:UIControlEventTouchUpInside];
@@ -298,15 +306,15 @@ NSString * const kOnboardActionButtonAccessibilityIdentifier = @"OnboardActionBu
 }
 
 - (UIImage *)thumbnailImageForVideo:(NSURL *)videoURL {
-    
+
     AVURLAsset *asset = [[AVURLAsset alloc] initWithURL:videoURL options:nil];
     AVAssetImageGenerator *assetIG = [[AVAssetImageGenerator alloc] initWithAsset:asset];
     assetIG.appliesPreferredTrackTransform = YES;
     assetIG.apertureMode = AVAssetImageGeneratorApertureModeEncodedPixels;
-    
+
     NSError *error = nil;
     CGImageRef thumbnailImageRef = [assetIG copyCGImageAtTime:CMTimeMake(0, 60) actualTime:NULL error:&error];
-    
+
     if (!error) {
         UIImage *thumbnailImage = [[UIImage alloc] initWithCGImage:thumbnailImageRef];
         return thumbnailImage;
@@ -340,7 +348,7 @@ NSString * const kOnboardActionButtonAccessibilityIdentifier = @"OnboardActionBu
     if (self.movesToNextViewController) {
         [self.delegate moveNextPage];
     }
-    
+
     // call the provided action handler
     if (_buttonActionHandler) {
         _buttonActionHandler(self.delegate);
