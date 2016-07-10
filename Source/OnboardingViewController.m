5bf4380220820140604731ada449683dd39f485f
Mike Amaral <mike.amaral@ping4.com>
2016-07-09 22:15:14 -0400

    Use self for properties instead of instance var convention for consistency.

--- Source/OnboardingViewController.m
+++ Source/OnboardingViewController.m
@@ -131,7 +131,7 @@ static NSString * const kSkipButtonText = @"Skip";
     [super viewWillAppear:animated];
     
     // if we have a video URL, start playing
-    if (_videoURL) {
+    if (self.videoURL) {
         [self.moviePlayerController play];
     }
 }
@@ -146,11 +146,11 @@ static NSString * const kSkipButtonText = @"Skip";
 
 - (void)generateView {
     // create our page view controller
-    _pageVC = [[UIPageViewController alloc] initWithTransitionStyle:UIPageViewControllerTransitionStyleScroll navigationOrientation:UIPageViewControllerNavigationOrientationHorizontal options:nil];
-    _pageVC.view.frame = self.view.frame;
-    _pageVC.view.backgroundColor = [UIColor whiteColor];
-    _pageVC.delegate = self;
-    _pageVC.dataSource = self.swipingEnabled ? self : nil;
+    self.pageVC = [[UIPageViewController alloc] initWithTransitionStyle:UIPageViewControllerTransitionStyleScroll navigationOrientation:UIPageViewControllerNavigationOrientationHorizontal options:nil];
+    self.pageVC.view.frame = self.view.frame;
+    self.pageVC.view.backgroundColor = [UIColor whiteColor];
+    self.pageVC.delegate = self;
+    self.pageVC.dataSource = self.swipingEnabled ? self : nil;
     
     if (self.shouldBlurBackground) {
         [self blurBackground];
@@ -172,9 +172,9 @@ static NSString * const kSkipButtonText = @"Skip";
     // darkens it a bit for better contrast
     UIView *backgroundMaskView;
     if (self.shouldMaskBackground) {
-        backgroundMaskView = [[UIView alloc] initWithFrame:_pageVC.view.frame];
+        backgroundMaskView = [[UIView alloc] initWithFrame:self.pageVC.view.frame];
         backgroundMaskView.backgroundColor = [UIColor colorWithWhite:0.0 alpha:kBackgroundMaskAlpha];
-        [_pageVC.view addSubview:backgroundMaskView];
+        [self.pageVC.view addSubview:backgroundMaskView];
     }
 
     // set ourself as the delegate on all of the content views, to handle fading
@@ -187,27 +187,27 @@ static NSString * const kSkipButtonText = @"Skip";
     _currentPage = [self.viewControllers firstObject];
     
     // more page controller setup
-    [_pageVC setViewControllers:@[_currentPage] direction:UIPageViewControllerNavigationDirectionForward animated:YES completion:nil];
-    _pageVC.view.backgroundColor = [UIColor clearColor];
-    [self addChildViewController:_pageVC];
-    [self.view addSubview:_pageVC.view];
-    [_pageVC didMoveToParentViewController:self];
-    [_pageVC.view sendSubviewToBack:backgroundMaskView];
+    [self.pageVC setViewControllers:@[self.currentPage] direction:UIPageViewControllerNavigationDirectionForward animated:YES completion:nil];
+    self.pageVC.view.backgroundColor = [UIColor clearColor];
+    [self addChildViewController:self.pageVC];
+    [self.view addSubview:self.pageVC.view];
+    [self.pageVC didMoveToParentViewController:self];
+    [self.pageVC.view sendSubviewToBack:backgroundMaskView];
     
     // send the background image view to the back if we have one
     if (backgroundImageView) {
-        [_pageVC.view sendSubviewToBack:backgroundImageView];
+        [self.pageVC.view sendSubviewToBack:backgroundImageView];
     }
     
     // otherwise send the video view to the back if we have one
-    else if (_videoURL) {
-        self.moviePlayerController.contentURL = _videoURL;
-        self.moviePlayerController.view.frame = _pageVC.view.frame;
+    else if (self.videoURL) {
+        self.moviePlayerController.contentURL = self.videoURL;
+        self.moviePlayerController.view.frame = self.pageVC.view.frame;
         self.moviePlayerController.repeatMode = MPMovieRepeatModeOne;
         self.moviePlayerController.controlStyle = MPMovieControlStyleNone;
         
-        [_pageVC.view addSubview:self.moviePlayerController.view];
-        [_pageVC.view sendSubviewToBack:self.moviePlayerController.view];
+        [self.pageVC.view addSubview:self.moviePlayerController.view];
+        [self.pageVC.view sendSubviewToBack:self.moviePlayerController.view];
     }
     
     // create and configure the page control
@@ -224,7 +224,7 @@ static NSString * const kSkipButtonText = @"Skip";
     // so we can set ourself as the delegate, this is sort of hackish but the only current
     // solution I am aware of using a page view controller
     if (self.shouldFadeTransitions) {
-        for (UIView *view in _pageVC.view.subviews) {
+        for (UIView *view in self.pageVC.view.subviews) {
             if ([view isKindOfClass:[UIScrollView class]]) {
                 [(UIScrollView *)view setDelegate:self];
             }
@@ -407,7 +407,7 @@ static NSString * const kSkipButtonText = @"Skip";
     NSUInteger indexOfNextPage = [self.viewControllers indexOfObject:_currentPage] + 1;
     
     if (indexOfNextPage < self.viewControllers.count) {
-        [_pageVC setViewControllers:@[self.viewControllers[indexOfNextPage]] direction:UIPageViewControllerNavigationDirectionForward animated:YES completion:nil];
+        [self.pageVC setViewControllers:@[self.viewControllers[indexOfNextPage]] direction:UIPageViewControllerNavigationDirectionForward animated:YES completion:nil];
         [self.pageControl setCurrentPage:indexOfNextPage];
     }
 }
@@ -450,22 +450,22 @@ static NSString * const kSkipButtonText = @"Skip";
     // fade the page control to and from the last page
     if (self.fadePageControlOnLastPage) {
         if (transitioningToLastPage) {
-            _pageControl.alpha = percentCompleteInverse;
+            self.pageControl.alpha = percentCompleteInverse;
         }
 
         else if (transitioningFromLastPage) {
-            _pageControl.alpha = percentComplete;
+            self.pageControl.alpha = percentComplete;
         }
     }
 
     // fade the skip button to and from the last page
     if (self.fadeSkipButtonOnLastPage) {
         if (transitioningToLastPage) {
-            _skipButton.alpha = percentCompleteInverse;
+            self.skipButton.alpha = percentCompleteInverse;
         }
 
         else if (transitioningFromLastPage) {
-            _skipButton.alpha = percentComplete;
+            self.skipButton.alpha = percentComplete;
         }
     }
 }
